1ad5677e2e399ebbdb0f617df36e8d12cd52609f
Chanakyavarma17 <chanivarma17@gmail.com>
2026-03-24 09:51:30 +0530

    uc6

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,35 +1,31 @@
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TrainConsistManagementApp {
 
     public static void main(String[] args) {
 
         System.out.println("====================================");
-        System.out.println("UC5 - Preserve Insertion Order of Bogies");
+        System.out.println("UC6 - Map Bogie to Capacity (HashMap)");
         System.out.println("====================================\n");
 
-        // Create LinkedHashSet
-        Set<String> formation = new LinkedHashSet<>();
+        // Create HashMap
+        Map<String, Integer> capacityMap = new HashMap<>();
 
-        // Add bogies
-        formation.add("Engine");
-        formation.add("Sleeper");
-        formation.add("Cargo");
-        formation.add("Guard");
+        // Insert bogie capacities
+        capacityMap.put("First Class", 24);
+        capacityMap.put("Cargo", 120);
+        capacityMap.put("Sleeper", 72);
+        capacityMap.put("AC Chair", 56);
 
-        // Add duplicate (will be ignored)
-        formation.add("Sleeper");
+        // Display capacity details
+        System.out.println("Bogie Capacity Details:");
 
-        // Display final formation
-        System.out.println("Final Train Formation:");
-        System.out.println(formation);
-
-        // Note
-        System.out.println("\nNote:");
-        System.out.println("LinkedHashSet preserves insertion order and removes duplicates automatically.");
+        for (Map.Entry<String, Integer> entry : capacityMap.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
 
         // End message
-        System.out.println("\nUC5 formation setup completed...");
+        System.out.println("\nUC6 bogie-capacity mapping completed...");
     }
 }
