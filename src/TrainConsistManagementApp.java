82c0aa8f234e77324b8e820faa8cbedcc4e7022c
Chanakyavarma17 <chanivarma17@gmail.com>
2026-03-24 09:24:26 +0530

    uc1

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TrainConsistManagementApp {
+    public static void main(String[] args) {
+
+        // Title border
+        System.out.println("====================================");
+        System.out.println("=== Train Consist Management App ===");
+        System.out.println("====================================");
+
+        // Initialize train consist
+        List<String> trainConsist = new ArrayList<>();
+
+        // Messages
+        System.out.println("\nTrain initialized successfully...");
+        System.out.println("Initial Bogie Count : " + trainConsist.size());
+        System.out.println("Current Train Consist: " + trainConsist);
+
+        System.out.println("\nSystem ready for operations...");
+    }
 }
